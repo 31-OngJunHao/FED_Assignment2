81014b042a512186782fb10e9a353ea31539f4f5
JunHao-NP <S10262731@connect.np.edu.sg>
2024-02-07 02:53:39 +0800

    ScratchCard somewhat works just doesnt cover fully

--- script.js
+++ script.js
@@ -45,3 +45,45 @@ function getSelectedCountry() {
             console.error("Error during fetch operation:", error);
           });
 }
+//ScratchCard
+const createScratchCard = (canvasId, color) => {
+    let canvas = document.getElementById(canvasId);
+    let context = canvas.getContext("2d");
+
+const init = () => {
+    context.fillStyle = color;
+    context.fillRect(0,0,500,100);
+
+}
+
+let isDragging = false;
+
+const scratch = (x, y) => {
+    context.globalCompositeOperation = "destination-out";
+    context.beginPath();
+    context.arc(x,y,24,0, 2*Math.PI);
+    context.fill();
+}
+
+
+canvas.addEventListener("mousedown", (event) =>{
+isDragging = true;
+scratch(event.offsetX, event.offsetY);
+})
+
+canvas.addEventListener("mousemove", (event) => {
+    if(isDragging)
+    {
+        scratch(event.offsetX, event.offsetY);
+    }
+})
+
+canvas.addEventListener("mouseup", (event) => {
+    isDragging= false; 
+})
+canvas.addEventListener("mouseleave", (event) => {
+    isDragging = false;
+})
+init()
+};
+createScratchCard("scratch-card01", "white")
