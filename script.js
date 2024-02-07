dcc1a53b2fa78cfe859e2e99d3a4c8ee97c6be59
Jayme-Chua <s10257137@connect.np.edu.sg>
2024-02-07 11:34:23 +0800

    Change Sign up form to a pop up form in index.html

--- script.js
+++ script.js
@@ -87,3 +87,55 @@ canvas.addEventListener("mouseleave", (event) => {
 init()
 };
 createScratchCard("scratch-card01", "white")
+
+//Pop up sign up form
+function submitSignUpForm() {
+    document.addEventListener('DOMContentLoaded', function () {
+        //Get stored user data if any
+        const storedUserData = JSON.parse(localStorage.getItem('userData'));
+        
+        //Greet user
+        const userGreeting = document.getElementById('userGreeting');
+        if (storedUserData) {
+            //Write Username
+            userGreeting.textContent = `Hi, ${storedUserData.name}!`;
+        } else {
+            //Write anonymous if no username
+            userGreeting.textContent = 'Hi, Anonymous'; 
+        }
+        
+        //Get signUpform in signUp.html
+        const signUpForm = document.getElementById('signUpForm');
+        signUpForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+            
+            //Get values of name and email when button clicked
+            const name = document.getElementById('name').value;
+            const email = document.getElementById('email').value;
+    
+            if (name && email) {
+                const userData = {
+                    name: name,
+                    email: email
+                };
+            
+            //Save user data to local storage
+            localStorage.setItem('userData', JSON.stringify(userData));
+            userGreeting.textContent = `Hi, ${name}!`;
+            } else {
+                alert('Please fill in all fields with valid information.');
+            }
+        });
+    });
+    closeForm();
+}
+
+function openForm() {
+    document.getElementById("overlay").style.display = "flex";
+}
+
+function closeForm() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+document.getElementById("open-form-link").addEventListener("click", openForm);
