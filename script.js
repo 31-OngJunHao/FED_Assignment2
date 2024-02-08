514e926b54273e5a99ef530ee45215bfa9179335
Jayme-Chua <s10257137@connect.np.edu.sg>
2024-02-08 13:19:17 +0800

    Updated sign-up form to have a greeting with lottie animation using user data

--- script.js
+++ script.js
@@ -111,40 +111,43 @@ const observer = new IntersectionObserver((enteries) => {
           //Get stored user data if any
           const storedUserData = JSON.parse(localStorage.getItem('userData'));
   
-          //Greet user
-          const userGreeting = document.getElementById('userGreeting');
-          if (storedUserData) {
-              //Write Username
-              userGreeting.textContent = `Hi, ${storedUserData.name}!`;
-          } else {
-              //Write anonymous if no username
-              userGreeting.textContent = 'Hi, Anonymous'; 
-          }
-  
           //Get signUpform in signUp.html
           const signUpForm = document.getElementById('signUpForm');
           signUpForm.addEventListener('submit', function (event) {
               event.preventDefault();
-  
+            
               //Get values of name and email when button clicked
               const name = document.getElementById('name').value;
               const email = document.getElementById('email').value;
-  
+
+              //Save user data to local storage
               if (name && email) {
                   const userData = {
                       name: name,
                       email: email
                   };
-  
-              //Save user data to local storage
               localStorage.setItem('userData', JSON.stringify(userData));
-              userGreeting.textContent = `Hi, ${name}!`;
+              
+              
+              const userGreeting = document.getElementById('userGreeting');
+              userGreeting.textContent = `Hi, ${name}`;
+
               } else {
                   alert('Please fill in all fields with valid information.');
               }
           });
       });
-      closeForm();
+      //Make overlay(sign-up form) invisible
+      document.getElementById("overlay").style.display = "none";
+
+      //Display greeting in overlay2 greeting)
+      const name = document.getElementById('name').value;
+      const userGreeting = document.getElementById('userGreeting');
+      userGreeting.textContent = `Hi, ${name}`;
+    
+      //Make overlay2(greeting) visible and invisible after 3 seconds
+      document.getElementById("overlay2").style.display = "flex";
+      setTimeout(closethanks,3000);
   }
   
   function openForm() {
@@ -153,8 +156,6 @@ const observer = new IntersectionObserver((enteries) => {
   
   function closeForm() {
       document.getElementById("overlay").style.display = "none";
-      document.getElementById("overlay2").style.display = "flex";
-      setTimeout(closethanks,3000);
   }
   function closethanks(){
       document.getElementById("overlay2").style.display = "none";
