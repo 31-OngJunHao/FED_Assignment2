e637296bcc78f5c52e3bd2c20aacb1f29df56034
Jayme-Chua <s10257137@connect.np.edu.sg>
2024-02-08 15:13:14 +0800

    Debug error from validators and put images into images folder

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const observer = new IntersectionObserver((enteries) => {
     enteries.forEach((entry) =>{
-        console.log(entry)
+        console.log(entry);
         if(entry.isIntersecting){
             entry.target.classList.add('show');
         }
